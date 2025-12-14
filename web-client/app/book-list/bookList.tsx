152c87b33fdff8e365862987ecfa446771921dd8
Alex Rolfe <alex.rolfe27@gmail.com>
2025-12-14 07:22:21 -0600

    Actually fetch it from basic API

--- web-client/app/book-list/bookList.tsx
+++ web-client/app/book-list/bookList.tsx
@@ -8,21 +8,47 @@ export interface BookListProps {
 interface Book {
   isbn: string;
   title: string;
+  author: string;
+  publishedYear: string;
 }
 
 export function Booklist({ scannedIsbns }: BookListProps) {
   const [foundBooks, setFoundBooks] = useState<Book[]>([]);
 
   useEffect(() => {
+    const fetchBook = async (isbn: DetectedBarcode['rawValue']) => {
+      const found = await fetch(
+        `https://openlibrary.org/search.json?q=${latest}`,
+      );
+
+      if (found.ok) {
+        // just assume we get 1 back for now
+        const data = (await found.json()).docs.at(0);
+        console.info(`Debug Log - found the book:  ${JSON.stringify(data)}`);
+
+        const latestBook = {
+          title: data.title,
+          isbn,
+          author: data.author_name,
+          publishedYear: data.first_publish_year,
+        };
+        setFoundBooks([...foundBooks, latestBook]);
+      }
+    };
+
     const latest = scannedIsbns.at(-1);
     if (!!latest) {
-      const latestBook = { title: `random book ${latest}`, isbn: latest };
-      setFoundBooks([...foundBooks, latestBook]);
+      fetchBook(latest);
     }
   }, [scannedIsbns]);
 
   const bookList = foundBooks.map((book) => (
-    <li key={crypto.randomUUID()}>{book.title}</li>
+    <li key={crypto.randomUUID()}>
+      <p>
+        <b>{book.title}</b> {' | ' + book.author + ' | '} published in{' '}
+        {book.publishedYear}
+      </p>
+    </li>
   ));
 
   return (
