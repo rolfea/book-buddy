6f8faf0346d23a39c7ffbf21e3619370aa745f25
Alex Rolfe <alex.rolfe27@gmail.com>
2026-05-30 00:01:27 -0500

    Address some server-side vulns

--- server/internal/controller/response.go
+++ server/internal/controller/response.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -12,11 +13,15 @@ type ErrorResponse struct {
 func writeError(w http.ResponseWriter, status int, msg string) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(ErrorResponse{Error: msg})
+	if err := json.NewEncoder(w).Encode(ErrorResponse{Error: msg}); err != nil {
+		log.Printf("failed to write error response: %v", err)
+	}
 }
 
 func writeJSON(w http.ResponseWriter, status int, v interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(v)
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		log.Printf("failed to write JSON response: %v", err)
+	}
 }
